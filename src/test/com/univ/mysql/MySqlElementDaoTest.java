3bcebecf67aef7fba9dcdf8c9e61e9fec50e4a5e
OliaStfn <kardash44@mail.ru>
2017-12-08 22:52:08 +0200

    refactor db

---
+++ src/test/com/univ/mysql/MySqlElementDaoTest.java
@@ -0,0 +1,33 @@
+package com.univ.mysql;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MySqlElementDaoTest {
+    @Test
+    public void create() throws Exception {
+
+    }
+
+    @Test
+    public void read() throws Exception {
+
+    }
+
+    @Test
+    public void readAll() throws Exception {
+
+    }
+
+    @Test
+    public void update() throws Exception {
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+
+    }
+
+}
